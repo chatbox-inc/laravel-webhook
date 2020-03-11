34272a534a3990311911a59271e7ca40bec4e645
mikakane <t.goto@chatbox-inc.com>
2020-03-11 13:33:39 +0900

    udapte

--- src/WebhookEvent.php
+++ src/WebhookEvent.php
@@ -4,15 +4,18 @@
 namespace Chatbox\Laravel\Webhook;
 
 
+use Illuminate\Http\Request;
+
 class WebhookEvent
 {
+    /** @var Request */
     public $request;
 
     /**
      * WebhookEvent constructor.
      * @param $request
      */
-    public function __construct($request)
+    public function __construct(Request $request)
     {
         $this->request = $request;
     }
